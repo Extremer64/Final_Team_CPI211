813c2a34a30e4487ab7149efb7560bcc82c6cb11
Extremer64 <therainbowgibdo@gmail.com>
2022-03-27 17:22:04 -0700

    Game now has a lose sound and takes 5 seconds to reset the room

--- CPI211-GameJam3-Group8/Assets/Scripts/TimerSystem.cs
+++ CPI211-GameJam3-Group8/Assets/Scripts/TimerSystem.cs
@@ -9,39 +9,66 @@ public class TimerSystem : MonoBehaviour
     [SerializeField] public float timer;
     [SerializeField] public static float minutes;
     [SerializeField] public static float seconds;
+    public float timetoreset;
+    public bool Lose;
+    private AudioSource[] music;
+    private AudioSource background;
+    private AudioSource lose;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        timetoreset = 5;
+        Lose = false;
+        music = GetComponents<AudioSource>();
+        background = music[0];
+        lose = music[1];
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer -= Time.deltaTime;
-        minutes = Mathf.Floor(timer / 60);
-        seconds = Mathf.Floor(timer - (minutes * 60));
-        if (minutes == 0 && seconds < 10)
+        if (timer > 0)
         {
-            Timer.GetComponent<TMPro.TextMeshProUGUI>().text = "Time: 0:0" + seconds;
+            timer -= Time.deltaTime;
+            minutes = Mathf.Floor(timer / 60);
+            seconds = Mathf.Floor(timer - (minutes * 60));
+            if (minutes == 0 && seconds < 10)
+            {
+                Timer.GetComponent<TMPro.TextMeshProUGUI>().text = "Time: 0:0" + seconds;
+            }
+            else if (minutes == 0 && seconds >= 10)
+            {
+                Timer.GetComponent<TMPro.TextMeshProUGUI>().text = "Time: 0:" + seconds;
+            }
+            else if (minutes >= 1 && seconds < 10)
+            {
+                Timer.GetComponent<TMPro.TextMeshProUGUI>().text = "Time: " + minutes + ":0" + seconds;
+            }
+            else
+            {
+                Timer.GetComponent<TMPro.TextMeshProUGUI>().text = "Time: " + minutes + ":" + seconds;
+            }
         }
-        else if (minutes == 0 && seconds >= 10)
+        if (timer <= 0 && !Lose)
         {
-            Timer.GetComponent<TMPro.TextMeshProUGUI>().text = "Time: 0:" + seconds;
-        }
-        else if (minutes >= 1 && seconds < 10)
-        {
-            Timer.GetComponent<TMPro.TextMeshProUGUI>().text = "Time: " + minutes + ":0" + seconds;
-        }
-        else
-        {
-            Timer.GetComponent<TMPro.TextMeshProUGUI>().text = "Time: " + minutes + ":" + seconds;
+            Lose = true;
+            timer = 0;
+            minutes = 0;
+            seconds = 0;
+            background.Stop();
+            lose.Play();
         }
 
-        if (timer <= 0)
+        else if (Lose)
         {
-            Scene scene = SceneManager.GetActiveScene(); 
-            SceneManager.LoadScene(scene.name);
+            timetoreset -= Time.deltaTime;
+        }
+
+        if (timetoreset <= 0)
+        {
+                Scene scene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(scene.name);
         }
     }
 }
