04f7f1b1e09da1c6e9030ba630061c9670ae33e0
M4dk1ng <clm082401137@gmail.com>
2022-04-24 22:58:40 -0700

    Shadow/Basic AI
    Pushing final level and basic follow AI before something else shows up in the github

---
+++ CPI211-Final_project-Team_8/Assets/Scripts/onCollisionReload.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class onCollisionReload : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision) {
+        //Debug.Log("Test Col");
+        if(collision.gameObject.tag == "Enemy") {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
