d18e351e62364028599b02c674afbc00ff884bdd
Extremer64 <therainbowgibdo@gmail.com>
2022-04-27 16:49:55 -0700

    idk what these are but I'll send it

--- CPI211-Final_project-Team_8/Assets/Scripts/Keypad.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Keypad : MonoBehaviour
-{
-    public GameObject[] displayAndButtons = new GameObject[10];
-}
