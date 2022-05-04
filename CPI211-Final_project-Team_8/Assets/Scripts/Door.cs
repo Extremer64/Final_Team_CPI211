41a4621eb7b1a3d51513b6383564045ecaed8bce
jaymock7 <54039921+jaymock7@users.noreply.github.com>
2022-05-03 20:45:23 -0700

    maze level fully playable

--- CPI211-Final_project-Team_8/Assets/Scripts/Door.cs
+++ CPI211-Final_project-Team_8/Assets/Scripts/Door.cs
@@ -5,20 +5,46 @@ using UnityEngine;
 public class Door : Interactable
 {
     private CameraFollow cameraFollow;
+    private AudioSource audioSource;
+    public bool isCorrect;
+    public AudioClip audioClip;
     PlayerHandler pHandle;
     public GameObject player;
     private float newZ;
 
     void Start() { 
         interactTag = "MazeDoor";
-        player = FindObjectOfType<PlayerHandler>().gameObject;
-        pHandle = gameObject.GetComponent<PlayerHandler>();
+        pHandle = FindObjectOfType<PlayerHandler>();
+        player = pHandle.gameObject;
+        if (isCorrect)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.spatialBlend = 1.0f;
+            audioSource.loop = true;
+            audioSource.clip = audioClip;
+            audioSource.priority = 0;
+        }
     }
 
     void Update()
     {
         newZ = player.transform.position.z - 20;
         //Debug.Log(newZ);
+        if (isCorrect)
+        {
+            if(Vector3.Distance(audioSource.transform.position, player.transform.position) < 25.0f)
+            {
+                audioSource.volume = 0.1f + (1.0f / Vector3.Distance(audioSource.transform.position, player.transform.position));
+            }
+            else
+            {
+                audioSource.volume = 0.0f;
+            }
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
     }
 
     public override void Interact()
@@ -40,7 +66,7 @@ public class Door : Interactable
         yield return new WaitForEndOfFrame();
         //player.transform.position = new Vector3(newZ, player.transform.position.y, player.transform.position.z);
         //player.transform.position = new Vector3(0, 0, 0);
-        pHandle.Teleport(new Vector3(0,0,0));
+        pHandle.Teleport(pHandle.transform.position + Vector3.forward * 20.0f);
         yield return new WaitForSeconds(.01f);
         //pHandle.disable = false;
     }
